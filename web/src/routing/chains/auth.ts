f2dde1e05d0bd0e26c7a6d73298db5578c824a41
Mark <shalpack@gmail.com>
2023-09-17 13:40:33 +0300

    Remove redundant home redirect

--- web/src/routing/chains/auth.ts
+++ web/src/routing/chains/auth.ts
@@ -1,14 +1,8 @@
-import { homeRoute, loginRoute } from "@/routing/shared";
-import { getSessionFx, unauthorizedTriggered } from "@/shared/auth";
+import { loginRoute } from "@/routing/shared";
+import { unauthorizedTriggered } from "@/shared/auth";
 import { sample } from "effector";
 
 sample({
   clock: unauthorizedTriggered,
   target: loginRoute.open,
-});
-
-sample({
-  source: getSessionFx.doneData,
-  filter: (sessionResponse) => !!sessionResponse.data.token && sessionResponse.success,
-  target: homeRoute.open,
 });
