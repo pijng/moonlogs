48aec8627644202c2ef21d9f749af74f620e348c
Mark <shalpack@gmail.com>
2024-02-10 14:34:06 +0300

    Fix json decoder

--- internal/lib/serialize/serialize.go
+++ internal/lib/serialize/serialize.go
@@ -3,6 +3,8 @@ package serialize
 import (
 	"io"
 
+	stdjson "encoding/json"
+
 	"github.com/goccy/go-json"
 )
 
@@ -10,8 +12,8 @@ func NewJSONEncoder(w io.Writer) *json.Encoder {
 	return json.NewEncoder(w)
 }
 
-func NewJSONDecoder(r io.Reader) *json.Decoder {
-	return json.NewDecoder(r)
+func NewJSONDecoder(r io.Reader) *stdjson.Decoder {
+	return stdjson.NewDecoder(r)
 }
 
 func JSONUnmarshal(data []byte, v any) error {
