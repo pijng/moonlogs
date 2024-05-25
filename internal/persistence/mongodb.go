fd8905041ea0bc4a4c4522043e33905d29a0fc46
Mark <shalpack@gmail.com>
2024-05-26 01:17:20 +0300

    Create index for schema_name key on records collection in mongo

--- internal/persistence/mongodb.go
+++ internal/persistence/mongodb.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -19,5 +20,26 @@ func initMongoDB(dataSourceName string) (*mongo.Client, error) {
 		return nil, fmt.Errorf("pinging mongo db: %w", err)
 	}
 
+	err = createIndexes(client)
+	if err != nil {
+		return nil, fmt.Errorf("creating indexes: %w", err)
+	}
+
 	return client, err
+}
+
+func createIndexes(client *mongo.Client) error {
+	collection := client.Database("moonlogs").Collection("records")
+
+	indexModel := mongo.IndexModel{
+		Keys:    bson.D{{Key: "schema_name", Value: 1}},
+		Options: options.Index().SetUnique(false),
+	}
+
+	_, err := collection.Indexes().CreateOne(context.Background(), indexModel)
+	if err != nil {
+		return fmt.Errorf("index `schema_name` on `records` collection: %w", err)
+	}
+
+	return nil
 }
