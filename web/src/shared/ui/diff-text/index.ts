6ae98df629058b722d72740e06a657e2893a3f74
Mark <shalpack@gmail.com>
2024-06-09 16:45:19 +0300

    Split diff text on two columns to compare

--- web/src/shared/ui/diff-text/index.ts
+++ web/src/shared/ui/diff-text/index.ts
@@ -1,6 +1,8 @@
+import { i18n } from "@/shared/lib/i18n";
 import { Change, diffWords } from "diff";
 import { Store, createEffect, createEvent, restore, sample } from "effector";
-import { h, list, node, remap } from "forest";
+import { h, list, node, remap, spec } from "forest";
+import { triggerTooltip } from "../general-tooltip";
 
 const compareFx = createEffect(({ oldText, newText }: { oldText: string | undefined; newText: string | undefined }): Change[] => {
   return diffWords(oldText || "", newText || "");
@@ -9,27 +11,84 @@ const compareFx = createEffect(({ oldText, newText }: { oldText: string | undefi
 const triggerDiff = createEvent();
 
 export const DiffText = ({ oldText, newText }: { oldText: Store<string>; newText: Store<string> }) => {
-  sample({
+  const diffFx = sample({
     source: [oldText, newText],
     clock: triggerDiff,
     fn: ([oldText, newText]) => ({ oldText, newText }),
     target: compareFx,
   });
 
-  const $changes = restore(compareFx, []);
+  const $before = restore(diffFx, []).map((parts) => parts.filter((p) => !p.added));
+  const $after = restore(diffFx, []).map((parts) => parts.filter((p) => !p.removed));
 
-  list($changes, ({ store: change }) => {
-    h("span", {
-      text: remap(change, "value"),
-      classList: {
-        "whitespace-pre-wrap": true,
-        "break-words": true,
-        "cursor-pointer": true,
-        "bg-green-200": change.map((c) => Boolean(c.added)),
-        "dark:bg-green-800": change.map((c) => Boolean(c.added)),
-        "bg-red-200": change.map((c) => Boolean(c.removed)),
-        "dark:bg-red-800": change.map((c) => Boolean(c.removed)),
-      },
+  const beforeClicked = createEvent<MouseEvent>();
+  const afterClicked = createEvent<MouseEvent>();
+  const copyTextFx = createEffect((clickedText: string) => {
+    return navigator.clipboard.writeText(clickedText);
+  });
+
+  sample({
+    source: i18n("miscellaneous.copied_to_clipboard"),
+    clock: copyTextFx.done,
+    fn: (text) => ({ text: text }),
+    target: triggerTooltip,
+  });
+
+  h("div", () => {
+    spec({ classList: ["grid", "grid-cols-2", "mt-3"] });
+
+    h("div", () => {
+      spec({
+        classList: ["border-r-2", "pr-4", "border-dashed"],
+        handler: { on: { click: beforeClicked } },
+      });
+
+      sample({
+        source: $before.map((parts) => parts.map((p) => p.value).join("")),
+        clock: beforeClicked,
+        filter: () => !Boolean(window.getSelection()?.toString()),
+        target: copyTextFx,
+      });
+
+      list($before, ({ store: before }) => {
+        h("span", {
+          text: remap(before, "value"),
+          classList: {
+            "whitespace-pre-wrap": true,
+            "break-words": true,
+            "cursor-pointer": true,
+            "bg-red-200": before.map((p) => Boolean(p.removed)),
+            "dark:bg-red-800": before.map((p) => Boolean(p.removed)),
+          },
+        });
+      });
+    });
+
+    h("div", () => {
+      spec({
+        classList: ["border-l-2", "pl-4", "border-dashed"],
+        handler: { on: { click: afterClicked } },
+      });
+
+      sample({
+        source: $after.map((parts) => parts.map((p) => p.value).join("")),
+        clock: afterClicked,
+        filter: () => !Boolean(window.getSelection()?.toString()),
+        target: copyTextFx,
+      });
+
+      list($after, ({ store: after }) => {
+        h("span", {
+          text: remap(after, "value"),
+          classList: {
+            "whitespace-pre-wrap": true,
+            "break-words": true,
+            "cursor-pointer": true,
+            "bg-green-200": after.map((p) => Boolean(p.added)),
+            "dark:bg-green-800": after.map((p) => Boolean(p.added)),
+          },
+        });
+      });
     });
   });
 
